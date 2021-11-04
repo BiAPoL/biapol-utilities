e4a00cca8cf35487a9d3996a5dcf68f54fb57b3d
Johannes Müller <johannes.mueller88@hotmail.de>
2021-11-04 10:43:29 +0100

    Update __init__.py

--- biapolutils/label/__init__.py
+++ biapolutils/label/__init__.py
@@ -7,8 +7,6 @@ Created on Wed Nov  3 12:03:15 2021
 DFG funded cluster of excellence "Physics of Life", TU Dresden, Dresden, Germany
 """
 
-__version__ = "0.0.1"
-
 from ._intersection_over_union import intersection_over_union
 from ._label_overlap_matrix import label_overlap_matrix
 from ._match_labels import match_labels,match_labels_stack
