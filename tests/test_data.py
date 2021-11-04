dd25b49296fbf01450d0b99e33b06248c8c85f49
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-11-04 21:06:54 +0100

    add labels_colormap

---
+++ tests/test_data.py
@@ -0,0 +1,15 @@
+import biapol_utilities as biao
+import numpy as np
+
+def test_labels_colormap():
+    lut = biao.labels_colormap()
+
+    print(lut.colors[0])
+    print(lut.colors[1])
+    print(lut.colors[1000])
+
+    tolerance = 0.0001
+    assert np.array_equal(lut.colors[0], [0, 0, 0])
+    assert np.allclose(lut.colors[1], [0.8916548, 0.45756748, 0.77818808], tolerance)
+    assert np.allclose(lut.colors[1000], [0.11423668, 0.98120855, 0.55004896], tolerance)
+
