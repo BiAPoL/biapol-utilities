5c795fc109d18efd77eacc60bc28a263bb31dc8b
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-11-04 18:01:38 +0100

    added reference to test

--- tests/test_match_labels.py
+++ tests/test_match_labels.py
@@ -3,13 +3,13 @@
 import numpy as np
 import biapol_utilities as biau
 
-def test_match_labels():
+def test_match_labels2():
     
     labels_x = np.asarray([1, 1, 1, 0, 0, 2, 2, 4, 4, 4, 0], dtype=np.uint8)
-    labels_y = np.asarray([1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 5], dtype=np.uint8)
-    
-    labels_y_matched = biau.label.match_labels(labels_x, labels_y)
-    
-    print(labels_y_matched)
+    labels_y = np.asarray([1, 1, 2, 0, 0, 3, 3, 4, 4, 4, 5], dtype=np.uint8)
 
-    assert False
+    reference_y_matched = np.asarray([1, 1, 1, 0, 0, 2, 2, 4, 4, 4, 5], dtype=np.uint8)
+
+    labels_y_matched = biau.label.match_labels(labels_x, labels_y)
+
+    assert np.array_equal(reference_y_matched, labels_y_matched)
