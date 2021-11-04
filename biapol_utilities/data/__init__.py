296eb6e1adf122b8421d5778d3ef4434518fbce3
Johannes Müller <johannes.mueller88@hotmail.de>
2021-11-04 10:48:52 +0100

    removed unnecessary comments

--- biapol_utilities/data/__init__.py
+++ biapol_utilities/data/__init__.py
@@ -1,8 +1,3 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Nov  4 09:14:33 2021
-
-@author: Johannes Müller, DFG Cluster of Excellence "Physics of Life", TU Dresden
-"""
 
 from ._data import blobs
--- biapol_utilities/label/__init__.py
+++ biapol_utilities/label/__init__.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  3 12:03:15 2021
-
-@author: Johannes Müller DFG Cluster of Excellence "Physics of Life", TU Dresden
-"""
 
 from ._intersection_over_union import intersection_over_union
 from ._label_overlap_matrix import label_overlap_matrix
