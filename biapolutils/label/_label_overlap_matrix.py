ab725ce59cbfccaae0396801c95c1f8f86006ddb
Johannes Müller <johannes.mueller88@hotmail.de>
2021-11-04 08:52:13 +0100

    renamed variables

--- biapolutils/label/_label_overlap_matrix.py
+++ biapolutils/label/_label_overlap_matrix.py
@@ -7,7 +7,7 @@ Created on Wed Nov  3 11:55:46 2021
 
 import numpy as np
 
-def label_overlap_matrix(x, y):
+def label_overlap_matrix(label_image_x, label_image_y):
     """ fast function to get pixel overlaps between masks in x and y 
     
     From: https://github.com/MouseLand/cellpose/blob/6fddd4da98219195a2d71041fb0e47cc69a4b3a6/cellpose/metrics.py#L130
@@ -27,16 +27,16 @@ def label_overlap_matrix(x, y):
     # put label arrays into standard form then flatten them 
 #     x = (utils.format_labels(x)).ravel()
 #     y = (utils.format_labels(y)).ravel()
-    x = x.ravel()
-    y = y.ravel()
+    label_image_x = label_image_x.ravel()
+    label_image_y = label_image_y.ravel()
     
     # preallocate a 'contact map' matrix
-    overlap = np.zeros((1+x.max(),1+y.max()), dtype=np.uint)
+    overlap = np.zeros((1 + label_image_x.max(), 1 + label_image_y.max()), dtype=np.uint)
     
     # loop over the labels in x and add to the corresponding
     # overlap entry. If label A in x and label B in y share P
     # pixels, then the resulting overlap is P
     # len(x)=len(y), the number of pixels in the whole image 
-    for i in range(len(x)):
-        overlap[x[i],y[i]] += 1
+    for i in range(len(label_image_x)):
+        overlap[label_image_x[i], label_image_y[i]] += 1
     return overlap
