1788dbf6b8fc23ae76cdf34abf150138a54112ff
Johannes Müller <johannes.mueller88@hotmail.de>
2021-11-05 11:58:53 +0100

    Create test_label_overlap_matrix.py

---
+++ tests/test_label_overlap_matrix.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import bibiapol_utilities as bputils
+import numpy as np
+
+
+def test_label_overlap_matrix():
+    
+    # create test arrays
+    a = np.asarray([[1, 1, 0],
+                    [1, 2, 0],
+                    [0, 1, 0]])
+    
+    b = np.asarray([[1, 2, 0],
+                    [1, 1, 0],
+                    [0, 1, 0]])
+    
+    
+    overlap = bputils.label.label_overlap_matrix(a, b)
+    
+    correct_result = np.asarray([[4, 0, 0],
+                                 [0, 3, 1],
+                                 [0, 1, 0]])
+    
+    match = [x == y for x, y in zip(overlap, correct_result)]
+    
+    assert(all(match))
