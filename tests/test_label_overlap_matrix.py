e52511b4c107ab5379c5b7380a046c490ddd7b84
Johannes Müller <johannes.mueller88@hotmail.de>
2021-11-05 12:09:33 +0100

    Update test_label_overlap_matrix.py
    simplified test

--- tests/test_label_overlap_matrix.py
+++ tests/test_label_overlap_matrix.py
@@ -8,20 +8,18 @@ def test_label_overlap_matrix():
     
     # create test arrays
     a = np.asarray([[1, 1, 0],
-                    [1, 2, 0],
-                    [0, 1, 0]])
+                    [1, 1, 2],
+                    [0, 2, 2]])
     
-    b = np.asarray([[1, 2, 0],
-                    [1, 1, 0],
-                    [0, 1, 0]])
+    b = np.asarray([[1, 1, 0],
+                    [1, 2, 2],
+                    [0, 2, 2]])
     
     
     overlap = bputils.label.label_overlap_matrix(a, b)
     
-    correct_result = np.asarray([[4, 0, 0],
+    correct_result = np.asarray([[2, 0, 0],
                                  [0, 3, 1],
-                                 [0, 1, 0]])
+                                 [0, 0, 3]])
     
-    match = [x == y for x, y in zip(overlap, correct_result)]
-    
-    assert(all(match))
+    assert(np.array_equal(overlap, correct_result))
