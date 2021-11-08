a7d00ae7fc181afa10df9a87a53e9d06ee2f93a6
Johannes Müller <johannes.mueller88@hotmail.de>
2021-11-08 16:27:22 +0100

    added sklearn version and changed old function version to legacy function

--- biapol_utilities/label/_label_overlap_matrix.py
+++ biapol_utilities/label/_label_overlap_matrix.py
@@ -3,10 +3,12 @@
 import numpy as np
 from sklearn import metrics
 
-def label_overlap_matrix(label_image_x, label_image_y):
-    """ Get pixel overlaps between masks in x and y
+def label_overlap_matrix_numpy(label_image_x, label_image_y):
+    """ 
+    Legacy function to calculate label overlaps between masks in x and y.
+    May be deprecated in future versions of biapol_utilities.
     
-    From: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
+    From: https://github.com/MouseLand/cellpose/blob/6fddd4da98219195a2d71041fb0e47cc69a4b3a6/cellpose/metrics.py#L130
     
     Parameters
     ----------
@@ -24,6 +26,42 @@ def label_overlap_matrix(label_image_x, label_image_y):
     label_image_x = label_image_x.ravel()
     label_image_y = label_image_y.ravel()
     
+    overlap = np.zeros((1 + label_image_x.max(), 1 + label_image_y.max()), dtype=np.uint)
+    
+    for i in range(len(label_image_x)):
+        overlap[label_image_x[i], label_image_y[i]] += 1
+    
+    return overlap
+
+def label_overlap_matrix(label_image_x, label_image_y):
+    
+    """ Get pixel overlaps between masks in x and y
+    
+    From: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
+    
+    Parameters
+    ----------
+    x: ND-array, int
+        where 0=NO masks; 1,2... are mask labels
+    y: ND-array, int
+        where 0=NO masks; 1,2... are mask labels
+
+    Returns
+    -------
+    overlap: ND-array, int
+        matrix of pixel overlaps of size [x.max()+1, y.max()+1]
+    """
+    
+    
+    # Flatten input arrays
+    label_image_x = label_image_x.ravel()
+    label_image_y = label_image_y.ravel()
+    
+    # Calculate confusion matrix
     overlap = metrics.confusion_matrix(label_image_x, label_image_y)
+    
+    # Remove empty rows/columns
+    overlap = overlap[~np.all(overlap == 0, axis=1)]
+    overlap = overlap[:, ~np.all(overlap == 0, axis=0)]
     
     return overlap
