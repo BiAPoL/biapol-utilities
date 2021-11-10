7ce85872b446b9d2b1629bb7a304fbc18c013bbe
Johannes Müller <johannes.mueller88@hotmail.de>
2021-11-10 17:17:49 +0100

    Fixed error in suppression procedure
    Values below threshold should be removed AND only maximal entries are kept

--- biapol_utilities/label/_filter_similarity_matrix.py
+++ biapol_utilities/label/_filter_similarity_matrix.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
-def suppression_threshold(matrix, threshold = 0.25):
+def suppressed_maximal(matrix, threshold = 0.25):
     """
-    Suppresses entries in a similarity matrix below a defined threshold
+    Suppresses entries in a similarity matrix below a defined threshold and removes
+    all non-maximal entries
 
     Parameters
     ----------
@@ -27,39 +28,10 @@ def suppression_threshold(matrix, threshold = 0.25):
         
     if threshold < 0 or threshold >= 1:
         raise ValueError(f'Provided threshold must be between 0 and 1 but was {threshold}')
+        # Keep only ious above threshold
     
+    matrix[matrix < threshold] = 0.0
+    matrix[matrix < matrix.max(axis=0)] = 0.0
     matrix[matrix < threshold] = 0
-    
-    return matrix
-
-def label_wise_maximum(matrix, axis=0):
-    """
-    Removes all non-maximal entries in the similarity matrix in a label-wise fashion.
-    This means that the maximal value along a specified axis of the similarity matrix is detected,
-    and all other entries are set to zero.
-
-    Parameters
-    ----------
-    matrix : 2D-array
-        Matrix containing the metric that defines the similarity of labels
-        in two images. The entry in similarity_matrix[i, j] corresponds
-        to the "similarity" between label i in in image X and label j in
-        an image Y
-    axis : int, optional
-        Axis along which maxima will be detected. The default is 0.
-
-    Returns
-    -------
-    matrix : 2D-array
-        Matrix containing the metric that defines the maximal possible similarity of labels
-        in two images.
-
-    """
-    
-    if not len(matrix.shape) == 2:
-        raise ValueError(f'Dimension of similarity matrix was expected NxD but was found {matrix.shape}')
-    
-    matrix[:, np.argmax(matrix, axis=axis)] = 0
-    matrix = 1.0 * (matrix > 0)
     
     return matrix
