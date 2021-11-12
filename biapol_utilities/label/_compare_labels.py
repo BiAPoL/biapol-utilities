786d1fb78d461cfb9d114533bc4b2b6a4d1b8fd1
Johannes Müller <johannes.mueller88@hotmail.de>
2021-11-12 11:25:14 +0100

    Added Dice score to output

--- biapol_utilities/label/_compare_labels.py
+++ biapol_utilities/label/_compare_labels.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 import numpy as np
 from biapol_utilities import measure
 import pandas as pd
@@ -8,7 +7,13 @@ import pandas as pd
 
 def compare_labels(label_image_x, label_image_y):
     """
-    Compares two label images regarding metrics from sklearn
+    Compares two label images label_image_x and label_image_y regarding the
+    label-wise Jaccard- and Dice scores.
+    The Jaccard-score is defined as the intersection over union of two labelled
+    images.
+    The Dice score S can be derived from the Jaccard-score J through the
+    following relation:
+    .. math:: S = \frac{2J}{1+J}.
 
     Parameters
     ----------
@@ -19,26 +24,30 @@ def compare_labels(label_image_x, label_image_y):
 
     Returns
     -------
-    pandas dataframe with columns [label, jaccard-score]. Each row corresponds
-    to the measured jaccard-score of the respective label.
+    pandas dataframe with columns `[label, jaccard_score, dice_score]. Each row
+    corresponds to the measured quantitiy (Jaccard- or Dice score) of the
+    respective label.
     """
-    
+
     # Flatten input
     label_image_x = label_image_x.flatten()
     label_image_y = label_image_y.flatten()
-    
+
     if not label_image_x.shape == label_image_y.shape:
-        raise Exception(f'Input images must have same dimension but have {label_image_x.shape} and {label_image_y.shape}')
-    
+        raise Exception('Input images must have same dimension but have '
+                        f'{label_image_x.shape} and {label_image_y.shape}')
+
     # get list of present labels in bothn images and allocate results
     labels = np.unique(np.hstack([label_image_x, label_image_y]))
-    df = pd.DataFrame(columns = ['label', 'jaccard_score'])
-    
+    df = pd.DataFrame(columns=['label', 'jaccard_score'])
+
     # calculate Jaccard score
     jc_score = measure.labelwise_jaccard_score(label_image_x,
                                                label_image_y)
-    
+    dc_score = (2 * jc_score) / (1 + jc_score)
+
     df['label'] = labels
     df['jaccard_score'] = jc_score
-    
+    df['dice_score'] = dc_score
+
     return df
