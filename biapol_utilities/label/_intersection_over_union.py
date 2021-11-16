8da58a60a8ac615dfeda91018a434c42c0d14a0a
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2021-11-16 15:08:40 +0100

    Updated docstring for sphinx compliance

--- biapol_utilities/label/_intersection_over_union.py
+++ biapol_utilities/label/_intersection_over_union.py
@@ -5,7 +5,8 @@ from sklearn import metrics
 
 
 def intersection_over_union_matrix(label_image_x, label_image_y):
-    """Generates a matrix with intersection over union of all mask pairs
+    """Generate a matrix with intersection over union of all label pairs.
+
     How it works:
     The overlap matrix is a lookup table of the area of intersection
     between each set of labels (true and predicted). The true labels
@@ -19,8 +20,7 @@ def intersection_over_union_matrix(label_image_x, label_image_y):
     column vectors gives a 2D array with the areas of every label pair
     added together. This is equivalent to the union of the label areas
     except for the duplicated overlap area, so the overlap matrix is
-    subtracted to find the union matrix.
-    From: https://github.com/MouseLand/cellpose/blob/6fddd4da98219195a2d71041fb0e47cc69a4b3a6/cellpose/metrics.py#L165
+    subtracted to find the union matrix. Implementation taken from [#]_
 
     Parameters
     ----------
@@ -28,15 +28,17 @@ def intersection_over_union_matrix(label_image_x, label_image_y):
         label image, where 0=background; 1,2... are label masks
     label_image_y: ND-array, int
         label image, where 0=background; 1,2... are label masks
+
     Returns
     -------
     iou: ND-array, float
         matrix of IOU pairs of size [x.max()+1, y.max()+1]
+
     See Also
     --------
-    [0] https://clij.github.io/clij2-docs/reference_generateJaccardIndexMatrix
+    .. [#] https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
+    .. [#] https://clij.github.io/clij2-docs/reference_generateJaccardIndexMatrix
     """
-
     # Calculate overlap matrix
     overlap = metrics.confusion_matrix(label_image_x.ravel(),
                                        label_image_y.ravel())
