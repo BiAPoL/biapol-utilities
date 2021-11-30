bb31d0445b5ff53f30c78203376593db348967e4
Johannes Müller <johannes.mueller88@hotmail.de>
2021-11-30 18:34:54 +0100

    Make argument functions public

--- biapol_utilities/label/__init__.py
+++ biapol_utilities/label/__init__.py
@@ -3,3 +3,5 @@
 from ._intersection_over_union import intersection_over_union_matrix
 from ._match_labels import match_labels, match_labels_stack
 from ._compare_labels import compare_labels
+from ._matching_algorithms import max_similarity
+from ._filter_similarity_matrix import suppressed_similarity
