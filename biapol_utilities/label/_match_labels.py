c5740fd61ac047079a4275ddbee210b54e2a8b80
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2021-12-02 14:40:39 +0100

    Update _match_labels.py

--- biapol_utilities/label/_match_labels.py
+++ biapol_utilities/label/_match_labels.py
@@ -2,14 +2,14 @@
 
 from skimage.segmentation import relabel_sequential
 from ._intersection_over_union import intersection_over_union_matrix
-from ._matching_algorithms import match_max_similarity
+from ._matching_algorithms import match_max_similarity, match_gale_shapley
 from ._filter_similarity_matrix import suppressed_similarity
 
 
 def match_labels_stack(label_stack,
                        metric_method=intersection_over_union_matrix,
                        filter_method=suppressed_similarity,
-                       matching_method=match_max_similarity):
+                       matching_method=match_gale_shapley):
     """
     Match labels from subsequent slices with specified method.
 
@@ -17,7 +17,7 @@ def match_labels_stack(label_stack,
     ----------
     label_stack : 3D-array, int
         Stack of 2D label images to be stitched with axis order ZYX
-    *metric_method*: callable, optional
+    metric_method: callable, optional
         Method to be used to generate a metric of similarity between labels
         in subsequent slices. Must return a matrix with
         `shape=(max(n, m), max(n, m))`, where n and m are the present labels
@@ -25,12 +25,12 @@ def match_labels_stack(label_stack,
         normalized to a range of [0, 1], where 0 and signify minimal or maximal
         similarity between two labels.
         Default is `intersection_over_union_matrix`
-    *filter_method*: callable, optional
+    filter_method: callable, optional
         Method to be used to filter values from the similarity matrix. This can
         help to speed up the matching process if, fo instance, entries in the
         similarity matrix below a defined threshold are set to zero. Default is
         `suppressed_similarity(similiarity_matrix, threshold=0.3)`
-    *matching_method* : callable, optional
+    matching_method : callable, optional
         Method to be used for matching the labels. This function is supposed
         to return a binary matrix with `shape=(n+1, m+1)` corresponding to
         `match=1`, `no_match=0` of the `n` labels in a given slice and
@@ -55,7 +55,7 @@ def match_labels_stack(label_stack,
 def match_labels(label_image_x, label_image_y,
                  metric_method=intersection_over_union_matrix,
                  filter_method=suppressed_similarity,
-                 matching_method=match_max_similarity):
+                 matching_method=match_gale_shapley):
     """
     Match labels in `label_image_y` with labels in `label_image_x`.
 
@@ -70,7 +70,7 @@ def match_labels(label_image_x, label_image_y,
         Image that should serve as a reference for label-matching
     label_image_y : nd-array
         Image the labels of which should be paired with labels from imageA
-    *metric*: callable, optional
+    metric_method: callable, optional
         Method to be used to generate a metric of similarity between labels
         in subsequent slices. Must return a matrix with
         `shape=(max(n, m), max(n, m))`, where n and m are the present labels
@@ -78,12 +78,12 @@ def match_labels(label_image_x, label_image_y,
         normalized to a range of [0, 1], where 0 and signify minimal or maximal
         similarity between two labels.
         Default is `intersection_over_union_matrix`
-    *filter*:ccallable, optional
+    filter_method: callable, optional
         Method to be used to filter values from the similarity matrix. This can
         help to speed up the matching process if, fo instance, entries in the
         similarity matrix below a defined threshold are set to zero. Default is
         `suppressed_similarity(similiarity_matrix, threshold=0.3)`
-    *matching* : callable, optional
+    matching_method : callable, optional
         Method to be used for matching the labels. This function is supposed
         to return a binary matrix with `shape=(n+1, m+1)` corresponding to
         `match=1`, `no_match=0` of the `n` labels in a given slice and
